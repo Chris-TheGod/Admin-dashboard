68bdd144e0e6caa20d9851d54e13d870c48d5d44
Kristjan Lääne <xonproject@gmail.com>
2024-06-14 18:47:47 +0300

    sidebar icons

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -9,23 +9,53 @@ import {
   CommandSeparator,
   CommandShortcut,
 } from '@/components/ui/command';
+import {
+  LayoutDashboard,
+  Newspaper,
+  Folders,
+  CreditCard,
+  Settings,
+  User,
+} from 'lucide-react';
+import Link from 'next/link';
 
 const Sidebar = () => {
   return (
-    <Command>
+    <Command className='bg-secondary rounded-none'>
       <CommandInput placeholder='Type a command or search...' />
       <CommandList>
         <CommandEmpty>No results found.</CommandEmpty>
         <CommandGroup heading='Suggestions'>
-          <CommandItem>Calendar</CommandItem>
-          <CommandItem>Search Emoji</CommandItem>
-          <CommandItem>Calculator</CommandItem>
+          <CommandItem>
+            <LayoutDashboard className='mr-2 h-4 w-4' />
+            <Link href='/'>Dashboard</Link>
+          </CommandItem>
+          <CommandItem>
+            <Newspaper className='mr-2 h-4 w-4' />
+            <Link href='/posts'>Posts</Link>
+          </CommandItem>
+          <CommandItem>
+            <Folders className='mr-2 h-4 w-4' />
+            <Link href='#'>Categories</Link>
+          </CommandItem>
         </CommandGroup>
         <CommandSeparator />
         <CommandGroup heading='Settings'>
-          <CommandItem>Profile</CommandItem>
-          <CommandItem>Billing</CommandItem>
-          <CommandItem>Settings</CommandItem>
+          <CommandItem>
+            <User className='mr-2 h-4 w-4' />
+            <span>Profile</span>
+            <CommandShortcut>⌘P</CommandShortcut>
+          </CommandItem>
+          <CommandItem>
+            <CreditCard className='mr-2 h-4 w-4' />
+            <span>Billing</span>
+            <CommandShortcut>⌘B</CommandShortcut>
+          </CommandItem>
+          <CommandItem>
+            <Settings className='mr-2 h-4 w-4' />
+            <span>Settings</span>
+            <CommandShortcut>⌘S</CommandShortcut>
+          </CommandItem>
         </CommandGroup>
       </CommandList>
     </Command>
