e1468acfc745bb80e23b7209c81b33c64baae51e
Kristjan Lääne <xonproject@gmail.com>
2024-06-14 19:07:09 +0300

    make cards dynamic

--- components/dashboard/DashboardCard.tsx
+++ components/dashboard/DashboardCard.tsx
@@ -1,17 +1,23 @@
 import { Card, CardContent } from '../ui/card';
-import { Newspaper } from 'lucide-react';
+import { LucideIcon } from 'lucide-react';
 
-export const DashboardCard = () => {
+interface DashboardCardProps {
+  title: string;
+  count: number;
+  icon: React.ReactElement<LucideIcon>;
+}
+
+export const DashboardCard = ({ title, count, icon }: DashboardCardProps) => {
   return (
     <Card className='bg-slate-100 dark:bg-stale-800 p-4 pb-0'>
       <CardContent>
         <h3 className='text-3xl text-center mb-4 font-bold text-slate-500 dark:text-slate-200'>
-          Posts
+          {title}
         </h3>
         <div className='flex gap-5 justify-center items-center'>
-          <Newspaper className='text-slate-500' size={72} />
+          {icon}
           <h3 className='text-5xl font-semibold text-slate-500 dark:text-slate-200'>
-            100
+            {count}
           </h3>
         </div>
       </CardContent>
