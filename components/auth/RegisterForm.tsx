f8a404c10ebf837e731983b0df96cdda0762c99c
Kristjan Lääne <xonproject@gmail.com>
2024-06-18 11:51:40 +0300

    register form

--- components/auth/RegisterForm.tsx
+++ components/auth/RegisterForm.tsx
@@ -1,5 +1,163 @@
-const RegisterForm = () => {
-  return <div>Register Form</div>;
+'use client';
+
+import * as z from 'zod';
+import { useForm } from 'react-hook-form';
+import { zodResolver } from '@hookform/resolvers/zod';
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from '@/components/ui/form';
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from '@/components/ui/card';
+import { Input } from '@/components/ui/input';
+import { Button } from '@/components/ui/button';
+import { useRouter } from 'next/navigation';
+
+const formSchema = z.object({
+  name: z.string().min(1, {
+    message: 'A name is required',
+  }),
+  email: z
+    .string()
+    .min(1, {
+      message: 'Email is required',
+    })
+    .email({
+      message: 'Please enter a valid email',
+    }),
+  password: z.string().min(1, {
+    message: 'Password is required',
+  }),
+  confirmPassword: z.string().min(1, {
+    message: 'You must confirm your password',
+  }),
+});
+
+const registerForm = () => {
+  const router = useRouter();
+
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      name: '',
+      email: '',
+      password: '',
+      confirmPassword: '',
+    },
+  });
+
+  const handleSubmit = (data: z.infer<typeof formSchema>) => {
+    router.push('/');
+  };
+
+  return (
+    <Card>
+      <CardHeader>
+        <CardTitle>Register</CardTitle>
+        <CardDescription>Register a new account</CardDescription>
+      </CardHeader>
+      <CardContent className='space-y-2'>
+        <Form {...form}>
+          <form
+            onSubmit={form.handleSubmit(handleSubmit)}
+            className='space-y-6'
+          >
+            <FormField
+              control={form.control}
+              name='name'
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel className='uppercase text-xs font-bold text-zinc-500 dark:text-white'>
+                    Name
+                  </FormLabel>
+                  <FormControl>
+                    <Input
+                      className='bg-slate-100 dark:bg-slate-500 border-0 focus-visible:ring-0 text-black dark:text-white focus-visible:ring-offset-0'
+                      placeholder='Enter name'
+                      {...field}
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+
+            <FormField
+              control={form.control}
+              name='email'
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel className='uppercase text-xs font-bold text-zinc-500 dark:text-white'>
+                    Email
+                  </FormLabel>
+                  <FormControl>
+                    <Input
+                      className='bg-slate-100 dark:bg-slate-500 border-0 focus-visible:ring-0 text-black dark:text-white focus-visible:ring-offset-0'
+                      placeholder='Enter email'
+                      {...field}
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+
+            <FormField
+              control={form.control}
+              name='password'
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel className='uppercase text-xs font-bold text-zinc-500 dark:text-white'>
+                    Password
+                  </FormLabel>
+                  <FormControl>
+                    <Input
+                      type='password'
+                      className='bg-slate-100 dark:bg-slate-500 border-0 focus-visible:ring-0 text-black dark:text-white focus-visible:ring-offset-0'
+                      placeholder='Enter password'
+                      {...field}
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+
+            <FormField
+              control={form.control}
+              name='password'
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel className='uppercase text-xs font-bold text-zinc-500 dark:text-white'>
+                    Confirm password
+                  </FormLabel>
+                  <FormControl>
+                    <Input
+                      type='confirmPassword'
+                      className='bg-slate-100 dark:bg-slate-500 border-0 focus-visible:ring-0 text-black dark:text-white focus-visible:ring-offset-0'
+                      placeholder='Confirm pasword'
+                      {...field}
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <Button className='w-full'>Sign In</Button>
+          </form>
+        </Form>
+      </CardContent>
+    </Card>
+  );
 };
 
-export default RegisterForm;
+export default registerForm;
